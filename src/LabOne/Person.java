79dff47c22dce035b903f0aba934ed40c63c112d
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-22 13:58:57 +0200

    validation of an email

--- src/LabOne/Person.java
+++ src/LabOne/Person.java
@@ -2,9 +2,11 @@ package LabOne;
 
 public  abstract class Person {
     private String name;
+    private String email;
 
-    public Person(String name) {
+    public Person(String name, String email) {
         this.name = name;
+        this.email =email;
 
     }
 
@@ -19,6 +21,18 @@ public  abstract class Person {
         this.name = name;
     }
 
-
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        if (email != null && !email.trim().isEmpty()) {
+            if (!email.contains("@")) {
+                throw new IllegalArgumentException("Invalid email");
+            }
+            this.email = email.trim();
+        } else {
+            this.email = null;
+        }
+    }
     public abstract double calculateTuition();
 }
