ca543b7c847b89d7a9121e626fed4364d9a0464a
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-22 14:02:47 +0200

    Add Students class with inheritance from Person

--- src/LabOne/Students.java
+++ src/LabOne/Students.java
@@ -1,4 +1,33 @@
 package LabOne;
+public class Students extends Person {
+    private  String studentID;
+    private  String department;
+    public  Students(String name, String email,String studentID, String department){
+        super(name, email);
 
-public class Students {
+
+        this.studentID = studentID;
+        this.department =department;
+
+
+    }
+
+    @Override
+    public double calculateTuition() {
+        return 0;
+    }
+    public String getStudentID(){
+        return studentID;
+    }
+    public  String getDepartment(){
+        return department;
+    }
+
+    public String getEmail(){
+        return super.getEmail();
+    }
+    public void setStudentID(String studentID){
+        this.studentID = studentID;
+    }
+
 }
