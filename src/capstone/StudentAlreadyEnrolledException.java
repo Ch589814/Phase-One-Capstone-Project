3897ae5256f100b4873550101e02fffb54fe93f5
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-22 20:45:47 +0200

    add StudentAlreadyEnrolledException

---
+++ src/capstone/StudentAlreadyEnrolledException.java
@@ -0,0 +1,7 @@
+package capstone;
+
+public class StudentAlreadyEnrolledException extends RuntimeException {
+    public StudentAlreadyEnrolledException(String message) {
+        super(message);
+    }
+}
